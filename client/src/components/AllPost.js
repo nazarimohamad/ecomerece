8b449c9fc1ea70d79f072300153e9a32dafc629d
Ryan Nazari <ryannazari21@gmail.com>
2019-11-22 16:54:15 +0200

    add image and go down arrow at allpost component in client

--- client/src/components/AllPost.js
+++ client/src/components/AllPost.js
@@ -1,19 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SvgIcon from '@material-ui/core/SvgIcon';
 import './style.scss';
 
 import img from "../images/shoe1.jpeg"
 class AllPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      lift: false
+    })
+  }
 
   componentDidMount() {
     this.props.getPosts();
+    setInterval(() => {
+      this.setState({
+        lift: !this.state.lift
+      })
+    }, 800);
   }
 
   render() {
+    const {lift} = this.state
     const { posts, getPosts } = this.props;
     return (
       <div className='allpost'>
         <div className='allpost_hero'>
           <h1>the best online shop for shoes</h1>
+          <div className={lift ? 'lift' : 'left'}>
+            <ExpandMoreIcon />
+            <ExpandMoreIcon />
+          </div>
         </div>
         <div className='container'>
           {posts.map(post => (
